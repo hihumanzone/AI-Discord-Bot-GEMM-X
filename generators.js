7b89da3d057efd33c10831ddab8869046a09139b
emoo man <101509991+hihumanzone@users.noreply.github.com>
2024-07-08 21:40:03 +0530

    Update generators.js

--- generators.js
+++ generators.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import EventSource from 'eventsource';
+import fs from 'fs';
 
-import config from './config.json' assert { type: 'json' };
+const config = JSON.parse(fs.readFileSync('config.json', 'utf-8'));
 const { bannerMusicGen, nevPrompt } = config;
 
 
@@ -309,6 +310,13 @@ const modelsData = {
     triggerId: 5,
     useSize: false,
     dataPattern: (prompt, randomDigit, width, height) => [prompt, nevPrompt, randomDigit, true, width, height, 5, 35]
+  },
+  "Kolors": {
+    url: "https://gokaygokay-kolors.hf.space",
+    fnIndex: 0,
+    triggerId: 23,
+    useSize: false,
+    dataPattern: (prompt, randomDigit, width, height) => [prompt, nevPrompt, width, height, 35, 5, 1, true, randomDigit]
   }
 };
 
@@ -412,4 +420,4 @@ export {
   generateWithPlayground,
   generateImage,
   generateWithDalle3
-};
+};};
